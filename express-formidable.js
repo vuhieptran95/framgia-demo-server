491554dd4fe4c3e42aa042d394e9090da37744ba
hiep <vuhiep.tran95@gmail.com>
2018-10-19 02:33:42 +0700

    app run on GGcloud

--- express-formidable.js
+++ express-formidable.js
@@ -9,15 +9,9 @@ const imageminWebp = require("imagemin-webp");
 const path = require("path");
 const app = express();
 const util = require("util");
+const cors = require("cors");
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+ app.use(cors());
 
 app.post("/users", async (req, res) => {
   // res.header("Access-Control-Allow-Origin", "*");
